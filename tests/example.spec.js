52642571514c8d28d03e6c2ca836b170e0416f6f
LeTuChiBao <bao.le@discoveryloft.com>
2023-12-10 11:40:52 +0700

    Install dotenv to setup local env

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,23 +1,28 @@
 import { test } from "@playwright/test"; // expect isn't being used
 const {SampleAppPage} = require("../models/sameple-app.model")
+import * as dotenv from 'dotenv'
+dotenv.config() 
 let username = process.env.USERNAME
 let password = process.env.PASSWORD
+
 test.describe.parallel('suite',()=> {
   test("login success", async ({ page }) => {
+    console.log(process.env.USERNAME)
+    console.log(username)
     const samepleAppPage = new SampleAppPage(page)
     await samepleAppPage.navigateToSampleApp()
     await samepleAppPage.fillUsernameField(username)
     await samepleAppPage.fillPasswordField(password)
     await samepleAppPage.clickLoginButton()
     await samepleAppPage.expectedLoginTextTobe(`Welcome, ${username}!`)
-    
+
   });
 
   test("wrong password", async ({ page }) => {
     const samepleAppPage = new SampleAppPage(page)
     await samepleAppPage.navigateToSampleApp()
     await samepleAppPage.fillUsernameField(username)
-    await samepleAppPage.fillPasswordField("123")
+    await samepleAppPage.fillPasswordField("Wrongpass")
     await samepleAppPage.clickLoginButton()
     await samepleAppPage.expectedLoginTextTobe("Invalid username/password")
   });
@@ -26,7 +31,7 @@ test.describe.parallel('suite',()=> {
     const samepleAppPage = new SampleAppPage(page)
     await samepleAppPage.navigateToSampleApp()
     await samepleAppPage.fillUsernameField("")
-    await samepleAppPage.fillPasswordField("")
+    await samepleAppPage.fillPasswordField(password)
     await samepleAppPage.clickLoginButton()
     await samepleAppPage.expectedLoginTextTobe("Invalid username/password")
   });
@@ -37,7 +42,7 @@ test.describe.parallel('suite',()=> {
     await samepleAppPage.fillUsernameField(username)
     await samepleAppPage.fillPasswordField(password)
     await samepleAppPage.clickLoginButton()
-    await samepleAppPage.expectedLoginTextTobe("Welcome, Chi Bao!")
+    await samepleAppPage.expectedLoginTextTobe(`Welcome, ${username}!`)
     await samepleAppPage.clickLogoutButton()
     await samepleAppPage.expectedLoginTextTobe("User logged out.")
   });
